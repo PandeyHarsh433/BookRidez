09df9d842c1fee2b73d3ddf7a901f6580a0d96fd
Harsh Pandey <72655424+PandeyHarsh433@users.noreply.github.com>
2024-06-02 15:38:35 +0530

    Update cookie.js

--- server/utils/cookie.js
+++ server/utils/cookie.js
@@ -1,35 +1,18 @@
 const setAuthCookie = (res, userId, role, token) => {
-  res.cookie("userId", userId, {
-    maxAge: 1000 * 60 * 60 * 24 * 30,
-    httpOnly: true,
-    secure: true,
-    domain: process.env.DOMAIN_URL,
-    sameSite: "None",
-  });
+  const domain = process.env.DOMAIN_URL;
 
-  res.cookie("token", token, {
+  const cookieOptions = {
     maxAge: 1000 * 60 * 60 * 24 * 30,
     httpOnly: true,
     secure: true,
-    domain: process.env.DOMAIN_URL,
+    domain: domain,
     sameSite: "None",
-  });
+  };
 
-  res.cookie("loggedIn", true, {
-    secure: true,
-    httpOnly: true,
-    domain: process.env.DOMAIN_URL,
-    maxAge: 1000 * 60 * 60 * 24 * 30,
-    sameSite: "None",
-  });
-
-  res.cookie("role", role, {
-    secure: true,
-    httpOnly: true,
-    domain: process.env.DOMAIN_URL,
-    maxAge: 1000 * 60 * 60 * 24 * 30,
-    sameSite: "None",
-  });
+  res.cookie("userId", userId, cookieOptions);
+  res.cookie("token", token, cookieOptions);
+  res.cookie("loggedIn", true, cookieOptions);
+  res.cookie("role", role, cookieOptions);
 };
 
 module.exports = setAuthCookie;
