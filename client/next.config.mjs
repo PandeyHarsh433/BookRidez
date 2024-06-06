5c9ac875ab747f10eb472d89b9e46390d6ed34f1
Harsh Pandey <72655424+PandeyHarsh433@users.noreply.github.com>
2024-06-06 21:24:54 +0530

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: "dist",
   images: {
     unoptimized: true,
-  }
+  },
 };
 export default nextConfig;
